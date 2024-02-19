47e0198444cebddba812fe3d6847049c7e2d68e1
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-02-19 19:31:26 +0200

    add icons

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ const portfolio = document.querySelector(".card-wrapper");
 
 let projects = [
   {
+    title: "Greencity",
+    picture: "./img/portfolio-img/greencity.png",
+    codeUrl:
+      "https://github.com/ita-social-projects/GreenCityClient/pulls?q=is%3Apr+is%3Aclosed+assignee%3AOksanaFilipchuk",
+    deployUrl: "",
+    backendUrl: "",
+  },
+  {
     title: "Async Race",
-    picture: "/img/portfolio-img/async-race.png",
+    picture: "./img/portfolio-img/async-race.png",
     codeUrl:
       "https://github.com/OksanaFilipchuk/async-race/tree/async-race-new",
     deployUrl: "https://oksanafilipchuk.github.io/async-race/",
     backendUrl: "https://async-race-xoj8.onrender.com/",
   },
   {
+    title: "Online shop",
+    picture: "./img/portfolio-img/shop.png",
+    codeUrl: "https://github.com/OksanaFilipchuk/book-shop",
+    deployUrl: "https://oksanafilipchuk.github.io/book-shop/main-page/",
+    backendUrl: "",
+  },
+  {
     title: "Online zoo",
-    picture: "/img/portfolio-img/animals.png",
+    picture: "./img/portfolio-img/animals.png",
     codeUrl: "https://github.com/OksanaFilipchuk/online-zoo",
     deployUrl: "https://oksanafilipchuk.github.io/online-zoo/pages/main/",
     backendUrl: "",
   },
   {
     title: "Css Slider",
-    picture: "/img/portfolio-img/cssSlider.png",
+    picture: "./img/portfolio-img/cssSlider.png",
     codeUrl: "https://github.com/OksanaFilipchuk/cssMemSlider",
     deployUrl:
       "https://oksanafilipchuk.github.io/cssMemSlider/cssMemSlider/index.html",
@@ -26,14 +41,14 @@ let projects = [
   },
   {
     title: "Game-algorithm",
-    picture: "/img/portfolio-img/game.png",
+    picture: "./img/portfolio-img/game.png",
     codeUrl: "https://github.com/OksanaFilipchuk/eldritch-codejam",
     deployUrl: "https://oksanafilipchuk.github.io/eldritch-codejam/",
     backendUrl: "",
   },
   {
     title: "Virtual keyboard",
-    picture: "/img/portfolio-img/keyboard.png",
+    picture: "./img/portfolio-img/keyboard.png",
     codeUrl:
       "https://github.com/OksanaFilipchuk/virtual-keyboard/tree/keyboard",
     deployUrl: "https://oksanafilipchuk.github.io/virtual-keyboard/",
@@ -41,28 +56,22 @@ let projects = [
   },
   {
     title: "Momentum",
-    picture: "/img/portfolio-img/momentum.png",
+    picture: "./img/portfolio-img/momentum.png",
     codeUrl: "https://github.com/OksanaFilipchuk/Momentum",
     deployUrl: "https://oksanafilipchuk.github.io/Momentum/",
     backendUrl: "",
   },
   {
     title: "Portfolio",
-    picture: "/img/portfolio-img/portfolio.png",
+    picture: "./img/portfolio-img/portfolio.png",
     codeUrl: "https://github.com/OksanaFilipchuk/portfolio",
     deployUrl: "https://oksanafilipchuk.github.io/portfolio/portfolio/",
     backendUrl: "",
   },
-  {
-    title: "Online shop",
-    picture: "/img/portfolio-img/shop.png",
-    codeUrl: "https://github.com/OksanaFilipchuk/book-shop",
-    deployUrl: "https://oksanafilipchuk.github.io/book-shop/main-page/",
-    backendUrl: "",
-  },
+
   {
     title: "Travel",
-    picture: "/img/portfolio-img/travel.png",
+    picture: "./img/portfolio-img/travel.png",
     codeUrl: "https://github.com/OksanaFilipchuk/Travel",
     deployUrl:
       "https://rolling-scopes-school.github.io/oksanafilipchuk-JSFEPRESCHOOL2022Q2/travel/",
@@ -71,7 +80,7 @@ let projects = [
 
   {
     title: "Weather",
-    picture: "/img/portfolio-img/weather.png",
+    picture: "./img/portfolio-img/weather.png",
     codeUrl: "https://github.com/OksanaFilipchuk/Weather",
     deployUrl: "https://incomparable-selkie-9577e4.netlify.app/",
     backendUrl: "",
@@ -89,20 +98,23 @@ projects.forEach((el) => {
   let title = document.createElement("h5");
   title.classList.add("card-title");
   title.textContent = el.title;
-
-  let appLink = document.createElement("a");
-  appLink.textContent = "App";
-  appLink.classList.add("card-link");
-  appLink.setAttribute("href", el.deployUrl);
-  appLink.setAttribute("target", "_blank");
+  cardBody.appendChild(title);
 
   let appCodeLink = document.createElement("a");
   appCodeLink.textContent = "Code";
   appCodeLink.classList.add("card-link");
   appCodeLink.setAttribute("href", el.codeUrl);
+  appCodeLink.setAttribute("target", "_blank");
 
-  cardBody.appendChild(title);
-  cardBody.appendChild(appLink);
+  if (el.deployUrl) {
+    let appLink = document.createElement("a");
+    appLink.textContent = "See";
+    appLink.classList.add("card-link");
+    appLink.setAttribute("href", el.deployUrl);
+    appLink.setAttribute("target", "_blank");
+    cardBody.appendChild(appLink);
+  }
+
   cardBody.appendChild(appCodeLink);
 
   card.appendChild(picture);
@@ -113,6 +125,7 @@ projects.forEach((el) => {
     backendLink.textContent = "Backend Part";
     backendLink.classList.add("card-link");
     backendLink.setAttribute("href", el.backendUrl);
+    backendLink.setAttribute("target", "_blank");
     cardBody.appendChild(backendLink);
   }
   portfolio.appendChild(card);
